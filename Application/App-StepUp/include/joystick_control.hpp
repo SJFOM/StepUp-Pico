f1fbff017b74850695b58c1746d2ea407014995b
Sam <samjfom@gmail.com>
2023-04-20 00:00:43 +0100

    feat(joystick): Startup code + xy data struct

--- Application/App-StepUp/include/joystick_control.hpp
+++ Application/App-StepUp/include/joystick_control.hpp
@@ -11,8 +11,21 @@
 // Control libraries
 #include "../../../Interfaces/ControlInterface.hpp"
 
+// ADC Joystick pins
 #define ADC_PIN_JOYSTICK_X (26U)
 #define ADC_PIN_JOYSTICK_Y (27U)
+
+// ADC homing position allowable threshold values
+#define ADC_MIDWAY_VALUE_RAW (1<<6U)
+#define ADC_HOME_THRESHOLD_RAW (100U)
+#define ADC_LOWER_HOME_THRESHOLD_RAW (ADC_MIDWAY_VALUE_RAW + ADC_HOME_THRESHOLD_RAW)
+#define ADC_UPPER_HOME_THRESHOLD_RAW (ADC_MIDWAY_VALUE_RAW + ADC_HOME_THRESHOLD_RAW)
+
+struct JoystickPosition
+{
+    int16_t x, y;
+    uint16_t x_offset, y_offset;
+};
 
 class JoystickControl : public ControlInterface
 {
@@ -27,6 +40,7 @@ protected:
 
 private:
     bool m_init_success;
+    struct JoystickPosition joystick_pos;
 };
 
 #endif  // JOYSTICK_CONTROL_H_
--- Application/App-StepUp/src/joystick_control.cpp
+++ Application/App-StepUp/src/joystick_control.cpp
@@ -12,6 +12,9 @@
 #include "../include/joystick_control.hpp"
 
 
+// 12-bit conversion, assume max value == ADC_VREF == 3.3 V
+constexpr float adc_conversion_factor = 3.3f / (1 << 12);
+
 JoystickControl::JoystickControl()
 {
     m_init_success = false;
@@ -32,6 +35,24 @@ bool JoystickControl::init()
         // Make sure GPIO is high-impedance, no pullups etc
         adc_gpio_init(ADC_PIN_JOYSTICK_X);
         adc_gpio_init(ADC_PIN_JOYSTICK_Y); 
+
+        adc_select_input(0);
+        joystick_pos.x_offset = adc_read();
+        
+        adc_select_input(1);
+        joystick_pos.y_offset = adc_read();
+
+        printf("X - Raw value: 0x%03x, voltage: %f V\n", joystick_pos.x_offset, joystick_pos.x_offset * adc_conversion_factor);
+        printf("Y - Raw value: 0x%03x, voltage: %f V\n", joystick_pos.y_offset, joystick_pos.y_offset * adc_conversion_factor);
+
+        if((joystick_pos.x_offset > ADC_LOWER_HOME_THRESHOLD_RAW && joystick_pos.x_offset < ADC_UPPER_HOME_THRESHOLD_RAW) 
+            && (joystick_pos.y_offset > ADC_LOWER_HOME_THRESHOLD_RAW && joystick_pos.y_offset < ADC_UPPER_HOME_THRESHOLD_RAW))
+        {
+            // We assume that the joystick is roughly centered and so its current position must be its "home" position
+            joystick_pos.x = 0;
+            joystick_pos.y = 0;
+            m_init_success = true;
+        }
     }
     return m_init_success;
 }
