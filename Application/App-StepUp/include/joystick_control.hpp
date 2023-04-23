eb9764e7934aa881bde6ab87a9e468d2a3b54166
Sam <samjfom@gmail.com>
2023-04-23 22:57:03 +0100

    feat(joystick): Initial working implementation!
    X+ axis has control over motor with 4 levels of speed controllable based
    on X-channel displacement.

--- Application/App-StepUp/include/joystick_control.hpp
+++ Application/App-StepUp/include/joystick_control.hpp
@@ -4,6 +4,10 @@
 // pico-sdk
 #include "pico/stdlib.h"  // Includes `hardware_gpio.h`
 #include "hardware/adc.h"
+#include "hardware/gpio.h"
+
+// pin includes
+#include "pins_definitions.h"
 
 // Common
 #include "../../../Common/utils.h"
@@ -11,20 +15,30 @@
 // Control libraries
 #include "../../../Interfaces/ControlInterface.hpp"
 
-// ADC Joystick pins
-#define ADC_PIN_JOYSTICK_X (26U)
-#define ADC_PIN_JOYSTICK_Y (27U)
-
 // ADC homing position allowable threshold values
-#define ADC_MIDWAY_VALUE_RAW (1<<6U)
+#define ADC_MIDWAY_VALUE_RAW (1<<11U) // 12 bit ADC, take mid-way value (half)
 #define ADC_HOME_THRESHOLD_RAW (100U)
 #define ADC_LOWER_HOME_THRESHOLD_RAW (ADC_MIDWAY_VALUE_RAW + ADC_HOME_THRESHOLD_RAW)
 #define ADC_UPPER_HOME_THRESHOLD_RAW (ADC_MIDWAY_VALUE_RAW + ADC_HOME_THRESHOLD_RAW)
 
 struct JoystickPosition
 {
-    int16_t x, y;
+    int16_t x_pos, y_pos;
     uint16_t x_offset, y_offset;
+};
+
+enum JoystickState
+{
+    JOYSTICK_STATE_IDLE = 0,
+    JOYSTICK_STATE_LOW,
+    JOYSTICK_STATE_MID,
+    JOYSTICK_STATE_HIGH,
+};
+struct JoystickData
+{
+    JoystickPosition x_stage, y_stage;
+    enum JoystickState state;
+    bool button_is_pressed;
 };
 
 class JoystickControl : public ControlInterface
@@ -35,12 +49,13 @@ public:
     bool init();
     void deinit();
     void processJob(uint32_t tick_count);
+    enum JoystickState getJoystickState();
 
 protected:
 
 private:
     bool m_init_success;
-    struct JoystickPosition joystick_pos;
+    struct JoystickData m_joystick;
 };
 
 #endif  // JOYSTICK_CONTROL_H_
