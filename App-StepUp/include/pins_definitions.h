9764cd75a73bba2d31d69488f2c592713781b003
Sam <samjfom@gmail.com>
2024-06-26 21:58:43 +0100

    feat(pcb): Pinout updates, board worksgcngcn

--- App-StepUp/include/pins_definitions.h
+++ App-StepUp/include/pins_definitions.h
@@ -4,8 +4,9 @@
 /***************************/
 /* LED Status pins - START */
 /***************************/
-#define LED_PIN_YELLOW (17U)
-#define LED_PIN_GREEN  (20U)
+#define LED_PIN_RED   (2U)
+#define LED_PIN_GREEN (3U)
+#define LED_PIN_BLUE  (5U)
 
 /*************************/
 /* LED Status pins - END */
@@ -17,15 +18,17 @@
 
 // We are using pins 0 and 1, but see the GPIO function select table in the
 // datasheet for information on which other pins can be used.
-#define TMC_UART_TX_PIN (4U)
-#define TMC_UART_RX_PIN (5U)
+#define TMC_UART_TX_PIN (25U)
+#define TMC_UART_RX_PIN (24U)
 
 // Motor control pins
-#define TMC_ENABLE_PIN    (14U)
-#define TMC_N_STANDBY_PIN (15U)
+#define TMC_ENABLE_PIN    (20U)
+#define TMC_N_STANDBY_PIN (21U)
 
 // Diagnostics pins
-#define TMC_DIAG_PIN (16U)
+#define TMC_DIAG_PIN (19U)
+
+#define TMC_BOOST_EN (18U)
 
 /**********************/
 /* TMC2300 pins - END */
@@ -36,14 +39,14 @@
 /*************************/
 
 // Joystick ADC pins
-#define JOYSTICK_ADC_PIN_X            (26U)
-#define JOYSTICK_ADC_CHANNEL_X        (0U)
-#define JOYSTICK_ADC_PIN_Y            (27U)
-#define JOYSTICK_ADC_CHANNEL_Y        (1U)
+#define JOYSTICK_ADC_PIN_X            (29U)
+#define JOYSTICK_ADC_CHANNEL_X        (3U)
+#define JOYSTICK_ADC_PIN_Y            (28U)
+#define JOYSTICK_ADC_CHANNEL_Y        (2U)
 #define JOYSTICK_ADC_ROUND_ROBIN_MASK (0x03)
 
 // Joystick Button pin
-#define JOYSTICK_BUTTON_PIN (22U)
+#define JOYSTICK_BUTTON_PIN (17U)
 
 /***********************/
 /* Joystick pins - END */
