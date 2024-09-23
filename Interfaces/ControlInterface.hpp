3b07d7343334eb720d25c4d51fc785a6ff716fa3
Sam <samjfom@gmail.com>
2024-09-23 20:07:00 +0100

    feat(state): Implement peripheral control

--- Interfaces/ControlInterface.hpp
+++ Interfaces/ControlInterface.hpp
@@ -28,14 +28,38 @@ static const char *ControllerStateString[ControllerState::STATE_COUNT] = {
     "STATE_NEW_DATA",
 };
 
-class ControlInterface {
+class ControlInterface
+{
 public:
     virtual bool init() = 0;
     virtual void deinit() = 0;
+
+    /**
+     * @brief Control the functional state of the peripheral
+     *
+     * @param enable_disable
+     */
+    virtual void enableFunctionality(bool enable_disable)
+    {
+        m_is_enabled = enable_disable;
+    }
+
+    /**
+     * @brief Get peripherals functional state
+     *
+     * @return true if peripheral can be used
+     * @return false if peripheral should not yet be used
+     */
+    virtual bool isFunctionalityEnabled()
+    {
+        return m_is_enabled;
+    }
     virtual enum ControllerState processJob(uint32_t tick_count) = 0;
+
 protected:
 private:
     bool m_init_success;
+    bool m_is_enabled;
 };
 
-#endif // CONTROL_INTERFACE_H_
+#endif  // CONTROL_INTERFACE_H_
