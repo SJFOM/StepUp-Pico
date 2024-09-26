69d8bf0d9209a030ad8e15c07a53a418793ceb8b
Sam <samjfom@gmail.com>
2024-09-27 00:48:47 +0100

    feat(buzzer): Melody implementation wip

--- App-StepUp/include/buzzer_control.hpp
+++ App-StepUp/include/buzzer_control.hpp
@@ -26,6 +26,98 @@
 // Control libraries
 #include "../../../Interfaces/ControlInterface.hpp"
 
+#define MELODY_MAX_NOTE_COUNT (10U)
+
+enum BuzzerFunction
+{
+    BUZZER_OFF = 0U,
+    BUZZER_BOOT = 1U,
+    BUZZER_INFO = 2U,
+    BUZZER_WARN = 3U,
+    BUZZER_ERROR = 4U,
+    BUZZER_FUNC_MAX_COUNT
+};
+
+enum NoteDuration
+{
+    // These have all been prescribed as durations in milli-seconds
+    NOTE_WHOLE = 1000U,
+    NOTE_HALF = 500U,
+    NOTE_QUARTER = 250U,
+    NOTE_EIGHT = 125U,
+    NOTE_MAX_COUNT = 4U
+};
+
+enum NotePitch
+{
+    // These have all been prescribed assuming 8Khz = 65535. The range of notes
+    // are focused around a centre frequency of 4kHz given the buzzer used on
+    // the StepUp hardware resonates best at this frequency.
+    NOTE_OFF = 0,      // OHz = OFF
+    NOTE_B6 = 16183,   // 1976 Hz
+    NOTE_C7 = 17146,   // 2093 Hz
+    NOTE_Cs7 = 18165,  // 2217 Hz
+    NOTE_D7 = 19245,   // 2349 Hz
+    NOTE_Ds7 = 20390,  // 2489 Hz
+    NOTE_E7 = 21602,   // 2637 Hz
+    NOTE_F7 = 22887,   // 2794 Hz
+    NOTE_Fs7 = 24248,  // 2960 Hz
+    NOTE_G7 = 25689,   // 3136 Hz
+    NOTE_Gs7 = 27217,  // 3322 Hz
+    NOTE_A7 = 28835,   // 3520 Hz
+    NOTE_As7 = 30550,  // 3729 Hz
+    NOTE_B7 = 32367,   // 3951 Hz
+    NOTE_C8 = 34291,   // 4186 Hz
+    NOTE_Cs8 = 36330,  // 4435 Hz
+    NOTE_D8 = 38491,   // 4699 Hz
+    NOTE_Ds8 = 40779,  // 4978 Hz
+    NOTE_E8 = 43204,   // 5274 Hz
+    NOTE_F8 = 45773,   // 5588 Hz
+    NOTE_Fs8 = 48495,  // 5920 Hz
+    NOTE_G8 = 51379,   // 6272 Hz
+    NOTE_Gs8 = 54434,  // 6645 Hz
+    NOTE_A8 = 57671,   // 7040 Hz
+    NOTE_As8 = 61100,  // 7459 Hz
+    NOTE_B8 = 64733,   // 7902 Hz
+};
+
+struct Melody
+{
+    uint16_t note[MELODY_MAX_NOTE_COUNT];
+    uint16_t duration[MELODY_MAX_NOTE_COUNT];
+};
+
+static struct Melody melody_sweep_up = {
+    .note = {NotePitch::NOTE_G7,
+             NotePitch::NOTE_Gs7,
+             NotePitch::NOTE_A7,
+             NotePitch::NOTE_As7,
+             NotePitch::NOTE_B7,
+             NotePitch::NOTE_C8,
+             NotePitch::NOTE_Cs8,
+             NotePitch::NOTE_D8,
+             NotePitch::NOTE_Ds8},
+    .duration = {NoteDuration::NOTE_EIGHT,
+                 NoteDuration::NOTE_EIGHT,
+                 NoteDuration::NOTE_EIGHT,
+                 NoteDuration::NOTE_EIGHT,
+                 NoteDuration::NOTE_EIGHT,
+                 NoteDuration::NOTE_EIGHT,
+                 NoteDuration::NOTE_EIGHT,
+                 NoteDuration::NOTE_EIGHT,
+                 NoteDuration::NOTE_EIGHT,
+                 NoteDuration::NOTE_EIGHT}};
+
+static struct Melody melody_double_beep = {
+    .note = {NotePitch::NOTE_C8,
+             NotePitch::NOTE_OFF,
+             NotePitch::NOTE_C8,
+             NotePitch::NOTE_OFF},
+    .duration = {NoteDuration::NOTE_EIGHT,
+                 NoteDuration::NOTE_EIGHT,
+                 NoteDuration::NOTE_EIGHT,
+                 NoteDuration::NOTE_EIGHT}};
+
 class BuzzerControl : public ControlInterface
 {
 public:
@@ -34,11 +126,16 @@ public:
     bool init();
     void deinit();
     enum ControllerState processJob(uint32_t tick_count);
+    void setBuzzerFunction(enum BuzzerFunction buzzer_function);
 
 protected:
 private:
     bool m_init_success;
+    uint16_t m_pwm_slice_num;
+    enum ControllerState m_control_state;
+    enum BuzzerFunction m_buzzer_function;
     void setBuzzerFrequency(uint16_t frequency_in_hz);
+    void disableBuzzer();
 };
 
 #endif  // JOYSTICK_CONTROL_H_
--- App-StepUp/src/buzzer_control.cpp
+++ App-StepUp/src/buzzer_control.cpp
@@ -12,8 +12,14 @@
 
 #include "../include/buzzer_control.hpp"
 
+static volatile uint8_t s_note_index_in_melody = 0;
+static struct Melody *s_active_melody = nullptr;
+
+int64_t melody_timer_callback(alarm_id_t id, void *user_data);
+
 BuzzerControl::BuzzerControl()
 {
+    m_control_state = ControllerState::STATE_IDLE;
     m_init_success = false;
 }
 
@@ -28,7 +34,7 @@ bool BuzzerControl::init()
     gpio_set_function(BUZZER_PIN, GPIO_FUNC_PWM);
 
     // Find out which PWM slice is connected to GPIO 0 (it's slice 0)
-    uint slice_num = pwm_gpio_to_slice_num(BUZZER_PIN);
+    m_pwm_slice_num = pwm_gpio_to_slice_num(BUZZER_PIN);
 
     // Get some sensible defaults for the slice configuration. By default, the
     // counter is allowed to wrap over its maximum range (0 to 2**16-1)
@@ -41,13 +47,21 @@ bool BuzzerControl::init()
     setBuzzerFrequency(4000);
 
     // Set the PWM running
-    pwm_set_enabled(slice_num, true);
+    pwm_set_enabled(m_pwm_slice_num, true);
     sleep_ms(100);
-    pwm_set_enabled(slice_num, false);
+    pwm_set_enabled(m_pwm_slice_num, false);
     sleep_ms(100);
-    pwm_set_enabled(slice_num, true);
+    pwm_set_enabled(m_pwm_slice_num, true);
     sleep_ms(100);
-    pwm_set_enabled(slice_num, false);
+    pwm_set_enabled(m_pwm_slice_num, false);
+
+    // TODO: Ensure buzzer resets output DC signal to 0V once complete as a
+    // permanent DC bias on the buzzer can damage the piezo hardware. If this
+    // needs more than just setting pwm_set_enabled() to false then we should
+    // consider a separate method to handle this.
+    disableBuzzer();
+
+    m_control_state = ControllerState::STATE_READY;
 
     m_init_success = true;
     return m_init_success;
@@ -58,9 +72,82 @@ void BuzzerControl::deinit()
     m_init_success = false;
 }
 
+void BuzzerControl::setBuzzerFunction(enum BuzzerFunction buzzer_function)
+{
+    // TODO: Potentially need to guard against setting a new buzzer melody here
+    // if one is already playing. Although, we should be able to guard against
+    // this with proper ControllerState management from within the main.cpp
+    // thread loop
+
+    m_control_state = ControllerState::STATE_BUSY;
+
+    switch (buzzer_function)
+    {
+        case BuzzerFunction::BUZZER_BOOT:
+        {
+            s_active_melody = &melody_sweep_up;
+            break;
+        }
+        case BuzzerFunction::BUZZER_INFO:
+        {
+            s_active_melody = &melody_double_beep;
+            break;
+        }
+        case BuzzerFunction::BUZZER_WARN:
+        {
+            // TODO: Create appropriate melody
+            s_active_melody = &melody_double_beep;
+            break;
+        }
+        case BuzzerFunction::BUZZER_ERROR:
+        {
+            // TODO: Create appropriate melody
+            s_active_melody = &melody_double_beep;
+            break;
+        }
+        case BuzzerFunction::BUZZER_OFF:
+        default:
+        {
+            s_active_melody = nullptr;
+            // TODO: If we need to power down buzzer, do it here
+            m_control_state = ControllerState::STATE_READY;
+            break;
+        }
+    }
+
+    if (m_control_state == ControllerState::STATE_BUSY)
+    {
+        pwm_set_enabled(m_pwm_slice_num, true);
+
+        // Update with new tone
+        pwm_set_gpio_level(BUZZER_PIN,
+                           s_active_melody->note[s_note_index_in_melody]);
+
+        // Schedule next timeout/note play duration
+        add_alarm_in_ms(s_active_melody->duration[s_note_index_in_melody],
+                        melody_timer_callback,
+                        NULL,
+                        false);
+    }
+}
+
+void BuzzerControl::disableBuzzer()
+{
+    pwm_set_enabled(m_pwm_slice_num, false);
+    m_buzzer_function = BuzzerFunction::BUZZER_OFF;
+}
+
 enum ControllerState BuzzerControl::processJob(uint32_t tick_count)
 {
-    return ControllerState::STATE_READY;
+    if (m_control_state == ControllerState::STATE_BUSY &&
+        s_note_index_in_melody == 0)
+    {
+        disableBuzzer();
+        // Melody was playing but has now completed, update state to represent
+        // this
+        m_control_state = ControllerState::STATE_READY;
+    }
+    return m_control_state;
 }
 
 void BuzzerControl::setBuzzerFrequency(uint16_t frequency_in_hz)
@@ -75,4 +162,27 @@ void BuzzerControl::setBuzzerFrequency(uint16_t frequency_in_hz)
     // some of these values if needed
     uint16_t pwm_level = frequency_in_hz * 65535 / 8000;
     pwm_set_gpio_level(BUZZER_PIN, pwm_level);
+}
+
+int64_t melody_timer_callback(alarm_id_t id, void *user_data)
+{
+    if (s_note_index_in_melody++ < MELODY_MAX_NOTE_COUNT)
+    {
+        // Update with new tone
+        pwm_set_gpio_level(BUZZER_PIN,
+                           s_active_melody->note[s_note_index_in_melody]);
+
+        // Schedule next timeout/note play duration
+        // TODO: Experiment with the fire_if_past flag if experiencing issues...
+        // May need to add some error handling here if timer cannot be created
+        add_alarm_in_ms(s_active_melody->duration[s_note_index_in_melody],
+                        melody_timer_callback,
+                        NULL,
+                        false);
+    }
+    else
+    {
+        s_note_index_in_melody = 0;
+    }
+    return 0;
 }
