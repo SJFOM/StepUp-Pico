a1aefa3dc6a93f72257b0559ebe3ce4a274e87c2
Sam <samjfom@gmail.com>
2025-01-19 19:46:58 +0000

    feat(led): LED library working

--- App-StepUp/src/led_control.cpp
+++ App-StepUp/src/led_control.cpp
@@ -12,8 +12,19 @@
 
 #include "../include/led_control.hpp"
 
-LEDControl::LEDControl()
+static volatile uint8_t s_effect_index_in_transition = 0;
+static struct LEDEffect *s_active_effect = nullptr;
+
+static RGBPinHandlers s_rgb_led;
+
+int64_t ledTimerCallback(alarm_id_t id, void *user_data);
+void queueNextLEDEffect();
+
+LEDControl::LEDControl(uint led_red_pin, uint led_green_pin, uint led_blue_pin)
 {
+    s_rgb_led.led_pin_red = led_red_pin;
+    s_rgb_led.led_pin_green = led_green_pin;
+    s_rgb_led.led_pin_blue = led_blue_pin;
     m_control_state = ControllerState::STATE_IDLE;
     m_init_success = false;
 }
@@ -25,19 +36,13 @@ LEDControl::~LEDControl()
 
 bool LEDControl::init()
 {
-    // Configure the led pins and direction
-    gpio_set_function(LED_PIN_BLUE, GPIO_FUNC_PWM);
-
-    // Find out which PWM slice is connected to LED_PIN
-    m_pwm_slice_num = pwm_gpio_to_slice_num(LED_PIN_BLUE);
-
-    // Get some sensible defaults for the slice configuration. By default, the
-    // counter is allowed to wrap over its maximum range (0 to 2**16-1)
-    pwm_config config = pwm_get_default_config();
-
-    // Set divider, reduces counter clock to sysclock/this value (sysclock =
-    // 125MHz default), 125MHz / 15625 = 8kHz
-    pwm_config_set_clkdiv(&config, 15625.f);  // should give 8kHz div clk
+    s_rgb_led.led_red_pwm_slice =
+        Utils::configurePWMPin(s_rgb_led.led_pin_red, LED_BASE_PWM_FREQ_IN_HZ);
+    s_rgb_led.led_green_pwm_slice =
+        Utils::configurePWMPin(s_rgb_led.led_pin_green,
+                               LED_BASE_PWM_FREQ_IN_HZ);
+    s_rgb_led.led_blue_pwm_slice =
+        Utils::configurePWMPin(s_rgb_led.led_pin_blue, LED_BASE_PWM_FREQ_IN_HZ);
 
     disableLED();
 
@@ -56,15 +61,119 @@ void LEDControl::setLEDFunction(
     enum ControllerNotification controller_notification)
 
 {
-    ;
+    if (m_control_state == ControllerState::STATE_READY)
+    {
+        m_control_state = ControllerState::STATE_BUSY;
+
+        switch (controller_notification)
+        {
+            case ControllerNotification::NOTIFY_BOOT:
+            {
+                s_active_effect = &effect_fade_to_on;
+                s_rgb_led.led_pin_in_use = s_rgb_led.led_pin_green;
+                s_rgb_led.led_pwm_slice_in_use = s_rgb_led.led_green_pwm_slice;
+                break;
+            }
+            case ControllerNotification::NOTIFY_INFO:
+            {
+                s_active_effect = &effect_blink;
+                s_rgb_led.led_pin_in_use = s_rgb_led.led_pin_blue;
+                s_rgb_led.led_pwm_slice_in_use = s_rgb_led.led_blue_pwm_slice;
+                break;
+            }
+            case ControllerNotification::NOTIFY_WARN:
+            {
+                s_active_effect = &effect_blink;
+                s_rgb_led.led_pin_in_use = s_rgb_led.led_pin_red;
+                s_rgb_led.led_pwm_slice_in_use = s_rgb_led.led_red_pwm_slice;
+                break;
+            }
+            case ControllerNotification::NOTIFY_ERROR:
+            {
+                s_active_effect = &effect_rapid_blink;
+                s_rgb_led.led_pin_in_use = s_rgb_led.led_pin_red;
+                s_rgb_led.led_pwm_slice_in_use = s_rgb_led.led_red_pwm_slice;
+                break;
+            }
+            default:
+            {
+                s_active_effect = nullptr;
+                disableLED();
+                m_control_state = ControllerState::STATE_READY;
+                break;
+            }
+        }
+
+        if (m_control_state == ControllerState::STATE_BUSY)
+        {
+            // Enable the led
+            pwm_set_enabled(s_rgb_led.led_pwm_slice_in_use, true);
+
+            queueNextLEDEffect();
+        }
+    }
 }
 
 void LEDControl::disableLED()
 {
-    pwm_set_enabled(m_pwm_slice_num, false);
+    pwm_set_enabled(s_rgb_led.led_red_pwm_slice, false);
+    pwm_set_enabled(s_rgb_led.led_green_pwm_slice, false);
+    pwm_set_enabled(s_rgb_led.led_blue_pwm_slice, false);
 }
 
 enum ControllerState LEDControl::processJob(uint32_t tick_count)
 {
+    if (m_control_state == ControllerState::STATE_BUSY &&
+        s_effect_index_in_transition == LED_MAX_TRANSITION_COUNT)
+    {
+        disableLED();
+        s_effect_index_in_transition = 0;
+        s_active_effect = nullptr;
+        // LED transition was ongoing but has now completed, update state to
+        // represent this
+        m_control_state = ControllerState::STATE_READY;
+    }
     return m_control_state;
+}
+
+void queueNextLEDEffect()
+{
+    if (s_effect_index_in_transition < LED_MAX_TRANSITION_COUNT &&
+        s_active_effect != nullptr)
+    {
+        // Update with new tone
+        pwm_set_gpio_level(
+            s_rgb_led.led_pin_in_use,
+            s_active_effect->effect[s_effect_index_in_transition]);
+
+        // Schedule next timeout/effect show duration
+        // TODO: Experiment with the fire_if_past flag if experiencing issues...
+        // May need to add some error handling here if timer cannot be created
+        alarm_id_t alarm_id = add_alarm_in_ms(
+            s_active_effect->duration[s_effect_index_in_transition],
+            ledTimerCallback,
+            NULL,    // user_data
+            false);  // fire_if_past
+
+        // Only play the next effect if we have valid alarm id's to use.
+        // The alarm ID will be invalid (i.e. <= 0) if the timer duration is too
+        // short (e.g. 0ms), as is the case when a effect duration of 0ms is
+        // detected.
+        if (alarm_id > 0)
+        {
+            s_effect_index_in_transition++;
+        }
+        else
+        {
+            // Terminate the melody early
+            s_effect_index_in_transition = LED_MAX_TRANSITION_COUNT;
+        }
+    }
+}
+
+int64_t ledTimerCallback(alarm_id_t id, void *user_data)
+{
+    // TODO: Handle the alarm_id_t & user_data params
+    queueNextLEDEffect();
+    return 0;
 }
